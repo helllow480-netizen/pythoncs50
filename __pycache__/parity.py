51e84ac2cfc926d49cbc553b5c7b342aa37d21f3
Ricardo Tumale <helllow480@gmail.com>
2026-02-06 11:42:40 +0800

    lecture-3

--- __pycache__/parity.py
+++ __pycache__/parity.py
@@ -1,6 +1,14 @@
-x = int(input("what is x?"))
+def main():
+    x = int(input("whats x? "))
+    if is_even(x):
+        print("true")
+    else:
+        print("false")
 
-if x % 2 == 0:
-    print("even")  
-else:
-    print("odd")    
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+main()
